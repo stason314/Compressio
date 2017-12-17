a372345087e9632ff9706b97588dd42a5cb3e414
aDamn Sandel <stanislavak314@gmail.com>
2017-12-17 14:37:41 +0700

    little thing

--- src/com/AI/Gui.java
+++ src/com/AI/Gui.java
@@ -38,6 +38,7 @@ public class Gui {
                     if(fileChooser.isFileSelectionEnabled())
                         fileName = file.getPath();
                    appends();
+                    printProbabilities();
 
                     System.out.println(fileName);
                 }
